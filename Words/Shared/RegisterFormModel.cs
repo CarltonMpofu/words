b8b95a1b8734213141689daaaca0e29f407b4af9
Carlton Mpofu <mpofucarlton0@gmail.com>
2023-12-21 02:01:00 +0200

    Add validation to the form in registerPage

---
+++ Words/Shared/RegisterFormModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Words.Shared
+{
+    public class RegisterFormModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(30, ErrorMessage = "Password must be at least 8 characters long.", MinimumLength = 8)]
+        public string Password { get; set; } = string.Empty;
+
+        [Required]
+        [Compare(nameof(Password), ErrorMessage = "Passwords do not match")]
+        public string Password2 { get; set; } = string.Empty;
+    }
+}
