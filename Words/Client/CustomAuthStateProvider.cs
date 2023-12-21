576a5c2496b246ec46e64f3ce05a6164ef69911a
Carlton Mpofu <mpofucarlton0@gmail.com>
2023-12-21 22:26:26 +0200

    Authenticate user token and show some pages only when authenticated

---
+++ Words/Client/CustomAuthStateProvider.cs
@@ -0,0 +1,54 @@
+﻿using Blazored.LocalStorage;
+using System.Security.Claims;
+using System.Text.Json;
+
+namespace Words.Client
+{
+    public class CustomAuthStateProvider : AuthenticationStateProvider
+    {
+        public ILocalStorageService _localStorage { get; }
+
+        // Inject local storage
+        public CustomAuthStateProvider(ILocalStorageService localStorage)
+        {
+            _localStorage = localStorage;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            string token = await _localStorage.GetItemAsStringAsync("token");
+
+            var identity = new ClaimsIdentity(); // means user is not authorised since it is empty
+
+            if(!string.IsNullOrEmpty(token)) // try to authenticate user if token exists
+                identity = new ClaimsIdentity(ParseClaimsFromJwt(token), "jwt"); // means user is  authorised
+
+            var user = new ClaimsPrincipal(identity);
+            var state = new AuthenticationState(user);
+
+            NotifyAuthenticationStateChanged(Task.FromResult(state));
+
+            return state;
+        }
+
+
+        // Not my hand writing
+        public static IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
+        {
+            var payload = jwt.Split('.')[1];
+            var jsonBytes = ParseBase64WithoutPadding(payload);
+            var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
+            return keyValuePairs.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString()));
+        }
+
+        private static byte[] ParseBase64WithoutPadding(string base64)
+        {
+            switch (base64.Length % 4)
+            {
+                case 2: base64 += "=="; break;
+                case 3: base64 += "="; break;
+            }
+            return Convert.FromBase64String(base64);
+        }
+    }
+}
--- Words/Client/Program.cs
+++ Words/Client/Program.cs
@@ -1,6 +1,7 @@
+global using Microsoft.AspNetCore.Components.Authorization;
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using MudBlazor.Services;
 using Words.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -8,6 +9,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-//builder.Services.AddMudServices();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>(); // use our custom suthentication state provider
+builder.Services.AddAuthorizationCore();
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
